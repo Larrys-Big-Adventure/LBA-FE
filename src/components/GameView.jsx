c98b99c2542987cb6ebf383b87d051c9d5b2d185
LarrySimiyu <larry.simiyu@gmail.com>
2019-11-20 14:07:48 -0800

    game view basic created

--- src/components/GameView.jsx
+++ src/components/GameView.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import { View } from '../styles/GameView';
 const GameView = () => {
   return (
-    <div>
+    <View>
       {/* giant border around taking up most of the screen */}
       <h1>Enjoy</h1>
-    </div>
+    </View>
   );
 };
 
